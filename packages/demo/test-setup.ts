8692bf47244f03615da849e35fe40fce076e71f3
Brian Schiller <bgschiller@gmail.com>
2024-04-19 21:32:16 -0700

    Add comment illustrating where I can add the declaration

--- packages/demo/test-setup.ts
+++ packages/demo/test-setup.ts
@@ -1,1 +1,10 @@
 import "vitest-unsettled/extend-expect";
+
+// If I uncomment these, it works. But I don't want to require this
+// on the user's part.
+// import type { UnsettledMatcher } from "vitest-unsettled";
+
+// declare module "vitest" {
+//   export interface Assertion<T = any> extends UnsettledMatcher<T> {}
+//   export interface AsymmetricMatchersContaining extends UnsettledMatcher {}
+// }
